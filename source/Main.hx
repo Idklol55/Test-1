25cf17346b45fa5e98a10ed388ce405f0080abfc
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2025-12-04 17:01:18 +0700

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -111,7 +111,6 @@ class Main extends Sprite
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		// addChild(new FlxGame(1280, 720, TitleState, 60, 60, true, false));
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		//#if !mobile
